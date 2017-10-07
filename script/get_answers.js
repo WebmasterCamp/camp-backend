01c4cbdbcc32f0d39c425ca765a9b566f5d946b3
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-10-07 20:37:22 +0700

    Edit script

--- script/get_answers.js
+++ script/get_answers.js
@@ -3,6 +3,7 @@ import User from '../src/models/user';
 import Question from '../src/models/question';
 import config from 'config';
 import fs from 'fs';
+import moment from 'moment';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI || config.MONGODB_URI);
@@ -14,7 +15,12 @@ User
     users
       .map(user => ({
         answers: user.questions.specialQuestions.marketing.map(answer => answer.answer),
-        id: user._id
+        id: user._id,
+        academicYear: user.academicYear,
+        faculty: user.faculty,
+        department: user.department,
+        university: user.university,
+        age: Math.abs(moment(user.birthdate).diff(moment(), 'years'))
       }))
     )
   )
@@ -22,11 +28,18 @@ User
     1: user.answers[0],
     2: user.answers[1],
     3: user.answers[2],
-    id: user.id
+    id: user.id,
+    academicYear: user.academicYear,
+    faculty: user.faculty,
+    department: user.department,
+    university: user.university,
+    age: user.age
   })))
   .then(users => {
     const body = users.map(user => `
 ID:${user.id}
+ชั้นปี: ${user.academicYear} / อายุ ${user.age} ปี
+คณะ: ${user.faculty} สาขาวิชา: ${user.department} มหาวิทยาลัย: ${user.university}
 ข้อที่ 1\n${user[1]}
 ข้อที่ 2\n${user[2]}
 ข้อที่ 3\n${user[3]}
