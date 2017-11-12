e467a59321d6df37af29e117748de846f5f3d52a
MicroBenz <microbenz.prob@gmail.com>
2017-11-12 23:30:33 +0700

    CHANGE DEADLINE LA

--- src/middlewares/deadline.js
+++ src/middlewares/deadline.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 export const closeAfterDeadline = (req, res, next) => {
-  if (moment().isAfter('2017-11-13T03:00:00')) {
+  if (moment().isAfter('2017-11-13T00:00:00')) {
     return res.error('Registration is now close');
   }
   return next();
