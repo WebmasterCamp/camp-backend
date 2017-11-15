00f9d3f507468bc56db6391389cf17a8f41e220d
MicroBenz <microbenz.prob@gmail.com>
2017-11-15 19:55:25 +0700

    Get interviewer

--- src/routes/interview.js
+++ src/routes/interview.js
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+
+import { User } from '../models';
 import { confirm } from '../controllers/interview';
 import { singleUpload, validateConfirm } from '../middlewares';
 
 const router = Router();
 
-
-router.post('/confirm', singleUpload('slip', 'jpg', 'png', 'jpeg'), validateConfirm, confirm);
+router.get('/:refId', async (req, res) => {
+  try {
+    const user = await User.findOne({ interviewRef: req.params.refId })
+      .populate('questions');
+    return res.send(user);
+  } catch (e) {
+    return res.error(e);
+  }
+});
+// router.post('/confirm', singleUpload('slip', 'jpg', 'png', 'jpeg'), validateConfirm, confirm);
 
 export default router;
